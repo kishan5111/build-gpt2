bf5bc9c69a2c7b2a35cdcefb31b577228e0ad54b
kishan vavdara <kishanvavdara@gmail.com>
2025-04-21 15:12:56 +0530

    add dataloaderlite

--- train_gpt2.py
+++ train_gpt2.py
@@ -200,6 +200,7 @@ class GPT(nn.Module):
         return model
     
 
+
 #--------------------------------
 num_return_sequences = 5
 max_length = 50
@@ -208,6 +209,40 @@ max_length = 50
 model = GPT.from_pretrained("gpt2") #init from hf 
 model.eval()
 model.to("mps")
+
+class DataLoaderLite: 
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory 
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens, dtype=torch.long)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state 
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B * T + 1]
+        x = buf[:-1].view(B, T) #inputs 
+        y = buf[1:].view(B, T) #targets
+        # advance the position 
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + B * T >= len(self.tokens):
+            self.current_position = 0
+        return x, y
+    
+
+    
+
 
 
 
